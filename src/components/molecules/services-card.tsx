dd14cb6bdca005df5bd27de0947be872610c3238
Leandro Felix <leandro.felix@g4flex.com.br>
2024-07-06 20:29:33 -0300

    feat: adiciona componentes de servicos com dados 🚧

--- src/components/molecules/services-card.tsx
+++ src/components/molecules/services-card.tsx
@@ -6,7 +6,7 @@ interface ServicesCardProps {
 
 function ServicesCard({ title, description, icon }: ServicesCardProps) {
   return (
-    <div className="flex h-full w-60 flex-col gap-2 rounded-lg border border-white/20 bg-white/10 p-4 backdrop-blur-sm">
+    <div className="flex h-full w-[280px] flex-col gap-2 rounded-lg border border-white/20 bg-white/5 p-4 backdrop-blur-sm">
       <div className="h-12 w-12 rounded-md border border-blue-500 bg-blue-700/50 p-2 text-blue-500">{icon}</div>
       <span className="text-sm">{title}</span>
       <p className="text-xs">{description}</p>
--- src/components/organisms/services.tsx
+++ src/components/organisms/services.tsx
@@ -1,5 +1,7 @@
 import { FaInstagram } from 'react-icons/fa'
 import { ServicesCard } from '../molecules/services-card'
+import { BiVector } from 'react-icons/bi'
+import { LuPrinter } from 'react-icons/lu'
 
 function Services() {
   return (
@@ -11,13 +13,31 @@ function Services() {
             <span className="text-blue-500">Eu desenvolvo</span> o que a sua empresa precisa!
           </h4>
         </div>
-        <ServicesCard
-          title={'Mídias Sociais'}
-          description={
-            'Sou designer gráfico especialista em criação de posts para social media. Crio posts para Instagram, Facebook, Twitter, LinkedIn e outras plataformas. Produzo imagens, vídeos, infográficos, animações e outros elementos visuais que compõem o seu perfil nas redes sociais.'
-          }
-          icon={<FaInstagram size={30} />}
-        />
+        <div className="mt-10 flex justify-center">
+          <div className="flex w-3/4 max-w-[1200px] flex-col justify-between gap-4 md:flex-row">
+            <ServicesCard
+              title={'Mídias Sociais'}
+              description={
+                'Sou designer gráfico especialista em criação de posts para social media. Crio posts para Instagram, Facebook, Twitter, LinkedIn e outras plataformas. Produzo imagens, vídeos, infográficos, animações e outros elementos visuais que compõem o seu perfil nas redes sociais.'
+              }
+              icon={<FaInstagram size={30} />}
+            />
+            <ServicesCard
+              title={'Mídias Sociais'}
+              description={
+                'Sou designer gráfico especialista em criação de posts para social media. Crio posts para Instagram, Facebook, Twitter, LinkedIn e outras plataformas. Produzo imagens, vídeos, infográficos, animações e outros elementos visuais que compõem o seu perfil nas redes sociais.'
+              }
+              icon={<BiVector size={30} />}
+            />
+            <ServicesCard
+              title={'Materiais Gráficos'}
+              description={
+                'Além disso, também posso te ajudar com os materiais gráficos, transformando-os e deixando totalmente editáveis, para você levar para uma gráfica como, por exemplo, cartão de visita, flyers para eventos, cardápios, papéis timbrados e entre outros arquivos de papelaria em geral.'
+              }
+              icon={<LuPrinter size={30} />}
+            />
+          </div>
+        </div>
       </div>
     </>
   )
