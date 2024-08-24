ab0bf6b694f013c353b51f71ceae3db66534b788
Leandro Felix <leandro.felix@g4flex.com.br>
2024-08-23 23:32:25 -0300

    feat: adiciona sessao de clientes

--- src/components/organisms/clients.tsx
+++ src/components/organisms/clients.tsx
@@ -1,8 +1,26 @@
+import { SectionHeader } from '../molecules/section-header'
+import { Section } from './section'
+
 function Clients() {
+  const clients = ['client_1', 'client_1', 'client_1', 'client_1', 'client_1']
   return (
-    <div>
-      <h1>Clients</h1>
-    </div>
+    <Section>
+      <SectionHeader
+        bolder={'É confiável mesmo?'}
+        highlight={'Sim'}
+        complement={', esses estão alguns clientes meus que você já deve ter ouvido falar'}
+      />
+      <div className="flex items-center justify-center gap-8">
+        {clients.map((client) => (
+          <img
+            src={`/public/clients/${client}.png`}
+            alt={client}
+            key={client}
+            className="w-52 grayscale transition-all hover:grayscale-0"
+          />
+        ))}
+      </div>
+    </Section>
   )
 }
 
